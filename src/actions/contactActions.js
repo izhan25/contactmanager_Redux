f39756e93d1bdcdf0ca596f02815fbedac5676bd
izhan25 <izhan.yameen25@gmail.com>
2019-01-25 04:19:40 +0500

    initial commit

---
+++ src/actions/contactActions.js
@@ -0,0 +1,46 @@
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT, GET_CONTACT, UPDATE_CONTACT } from './types';
+import axios from 'axios';
+
+export const getContacts = () => async dispatch => {
+
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+    });
+}
+
+export const getContact = (id) => async dispatch => {
+
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    dispatch({
+        type: GET_CONTACT,
+        payload: res.data
+    });
+}
+
+export const deleteContact = (id) => {
+    return {
+        type: DELETE_CONTACT,
+        payload: id
+    }
+}
+
+export const addContact = (contact) => {
+    return {
+        type: ADD_CONTACT,
+        payload: contact
+    }
+}
+
+export const updateContact = (contact) => async dispatch => {
+
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${contact.id}`, contact);
+
+    dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+    });
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple app to manage contacts <b>MADE WITH REDUX</b></p>
+      <p className="text-secondary">Version 1.0.0</p>
+      <p className="text-secondary">Author</p>
+    </div>
+  );
+};
